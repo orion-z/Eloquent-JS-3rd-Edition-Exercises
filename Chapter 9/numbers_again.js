2238cba4ed7f355690ce728a8f0d0aa8cee5a6f9
orion-z <35630953+orion-z@users.noreply.github.com>
2018-02-20 21:47:44 +0100

    Create numbers_again.js
    Eloquent JS - Chapter 9 - Numbers again

---
+++ Chapter 9/numbers_again.js
@@ -0,0 +1,31 @@
+let number = /^(\+|-|)(\d+(\.\d*)?|\.\d+)([eE](\+|-|)\d+)?$/;
+
+/* 
+
+breaking it down:
+
+/         starting regex
+^         beginning of input
+
+(\+|-|)   (escaped) plus | or minus | or nothing
+
+(         
+\d+       one or more digits
+(\.\d*)?  zero or one (dot followed by zero or more digits)
+
+|         OR
+
+\.\d+     one or more (dot followed by one or more digits)
+)         (this block needs to be in)
+
+(         
+[eE]      either 'e' or 'E'
+(\+|-|)   (escaped) plus | or minus | or nothing
+\d+       one or more digits
+)?        zero or one of this block
+
+$         end of input
+/         ending regex
+;    
+
+*/
